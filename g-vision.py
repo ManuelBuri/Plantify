0eb1c190db51479d46d4b5df2459c9dd9ca99a42
manfredbeluga <manfred.beluga@gmail.com>
2018-05-31 11:12:39 +0200

    added some comments

--- g-vision.py
+++ g-vision.py
@@ -9,15 +9,19 @@ from tkinter import filedialog
 from google.cloud import vision
 from google.cloud.vision import types
 
+# Specify API Credentials
+# apikey.json is currently not shared in github (is in plantify.gitignore)
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "apikey.json"
 
+# Access Cloud Vision Functions
 vision_client = vision.ImageAnnotatorClient()
 
+# Allows to open finder/explorer to choose a picture
 root = tk.Tk()
 root.withdraw()
 
 file_name = filedialog.askopenfilename()
-#file_name = '28.jpg'
+
 
 with io.open(file_name,'rb') as image_file:
     content = image_file.read()
@@ -27,6 +31,6 @@ image = types.Image(content=content)
 response = vision_client.label_detection(image=image)
 labels = response.label_annotations
 
+# Prints labels and its probability
 for label in labels:
-    print(label.description)
-
+    print(label.description, label.score)
