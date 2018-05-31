e1f31cd934668c379324a97a58020a54dea972fc
manfredbeluga <manfred.beluga@gmail.com>
2018-05-31 14:33:39 +0200

    Output from GVision is now saved in pandas DataFrame

--- g-vision.py
+++ g-vision.py
@@ -5,9 +5,14 @@ import os
 
 import tkinter as tk
 from tkinter import filedialog
+from typing import List
 
 from google.cloud import vision
 from google.cloud.vision import types
+
+import pandas
+import numpy
+
 
 # Specify API Credentials
 # apikey.json is currently not shared in github (is in plantify.gitignore)
@@ -20,8 +25,11 @@ vision_client = vision.ImageAnnotatorClient()
 root = tk.Tk()
 root.withdraw()
 
-file_name = filedialog.askopenfilename()
+# Allows us to choose a picture via finder/explorer
+# file_name = filedialog.askopenfilename()
 
+# Allows us to choose a picture in the project directory
+file_name = 'basil.jpg'
 
 with io.open(file_name,'rb') as image_file:
     content = image_file.read()
@@ -31,6 +39,33 @@ image = types.Image(content=content)
 response = vision_client.label_detection(image=image)
 labels = response.label_annotations
 
-# Prints labels and its probability
+### Detects description and its probability
+# Output is stored in list "Image_Label"
+
+#Containers for our output from Google Cloud Vision
+plant_output_gvision = []
+score_output_gvision = []
+
+# Loop to get Google Cloud Vision
 for label in labels:
-    print(label.description, label.score)
+    #print(label.description, label.score)
+    plant_output_gvision.append(label.description)
+    score_output_gvision.append([label.score])
+
+# Container for final output from Google Cloud Vision in Pandas DataFrame format
+output_gvision = pandas.DataFrame()
+
+# Save Python lists as pandas series
+label_se = pandas.Series(plant_output_gvision)
+score_se = pandas.Series(score_output_gvision)
+
+# Generate final Pandas DataFrame
+output_gvision['Label'] = label_se.values
+output_gvision['Score'] = score_se.values
+
+
+
+
+
+
+
